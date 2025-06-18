98cef210a540a6b7812d31ea55f8fefe8586378b
Luizstoeberl <luiz.stoeberl@aluno.unc.br>
2025-06-18 12:48:59 -0300

    tela criar post

--- screens/PostsScreens.js
+++ screens/PostsScreens.js
@@ -1,14 +1,59 @@
+import { Dimensions, FlatList, TouchableOpacity } from "react-native";
 import { SafeAreaView, StyleSheet, Text } from "react-native";
 import { View } from "react-native-web";
+import AntDesign from '@expo/vector-icons/AntDesign';
 
+export default function PostsScreencs() {
 
-export default function PostsScreencs () {
+    const DATA = [
+        {
+            id: 1,
+            title: 'Primeiro foto'
+        },
+        {
+            id: 2,
+            title: 'Segundo foto'
+        },
+        {
+            id: 3,
+            title: 'Terceiro foto'
+        }
+    ]
 
-    return(
+    const Item = ({ title }) => (
+        <View style={styles.item}>
+            <Text style={styles.title}>{title}</Text>
+        </View>
+    );
+
+    const numColumns = 3;
+    const tamImage = Dimensions.get('window').width / numColumns;
+
+    return (
         <SafeAreaView style={styles.coontainer}>
-            <View>
-                <Text style={styles.title}> Criar posts </Text>
+            <Text style={styles.title}> Novo post </Text>
+
+            <View style={styles.menu}>
+                <TouchableOpacity>
+                    <Text style={styles.text}> Sair </Text>
+                </TouchableOpacity>
             </View>
+
+            <View>
+                <TouchableOpacity>
+                    <AntDesign name="camera" size={32} color="black" />
+                </TouchableOpacity>
+            </View>
+
+            <View>
+                <FlatList
+                    data={DATA}
+                    renderItem={({ item }) => <Item title={item.title} />}
+                    keyExtractor={item => item.id}
+                    numColumns={numColumns}
+                />
+            </View>
+
         </SafeAreaView>
     )
 }
@@ -25,5 +70,18 @@ const styles = StyleSheet.create({
         textAlign: 'center',
         marginBottom: 40,
         fontWeight: 'bold'
+    },
+    item: {
+        backgroundColor: '#f9c2ff',
+        padding: 20,
+        marginVertical: 8,
+        marginHorizontal: 16,
+    },
+    menu: {
+        justifyContent: "flex-start"
+    },
+    text: {
+        fontWeight: "semibold",
+        fontSize: 20
     }
 })
